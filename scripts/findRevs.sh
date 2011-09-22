ceb7c89bd70fae7367c9cc6ca166fc9a7d4207eb
mh1985 <mh1985@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2011-09-22 15:20:25 +0000

    de: added german files, and enabled for automatic detection.

--- scripts/findRevs.sh
+++ scripts/findRevs.sh
@@ -121,7 +121,7 @@ if [ "$endRev" == "" ]; then
     exit
 fi
 
-langs=(ar fi it ja nl pl tr)
+langs=(ar de fi it ja nl pl tr)
 for lang in ${langs[*]}; do
     echo "processing $lang"
     origFile=changes.t2t
