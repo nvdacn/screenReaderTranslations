faac3ddc913aeaa002a533899a99f0946406e2a8
CommitBot <CommitBot@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2011-10-02 13:19:36 +0000

    scripts: adding in section start line number in stats, and updated all stats to include this info.

--- scripts/stats.py
+++ scripts/stats.py
@@ -11,28 +11,30 @@ pars = 0
 tables = 0
 lists = 0
 id = ''
-for line in lines:
-    if line.startswith("<P>"):
+i = 0
+for i in range(len(lines)):
+    if lines[i].startswith("<P>"):
         pars +=1; continue
-    if line.startswith("<TABLE "):
+    if lines[i].startswith("<TABLE "):
         tables +=1; continue
-    if line.startswith("<OL>") or line.startswith("<UL>"):
+    if lines[i].startswith("<OL>") or lines[i].startswith("<UL>"):
         lists +=1; continue
-    PnewSec = newSec.match(line)
+    PnewSec = newSec.match(lines[i])
     if PnewSec:
         if id:
-            info.append((id, pars, tables, lists))
+            info.append((id, i+1, pars, tables, lists))
         pars = 0; tables = 0; lists = 0
         id = PnewSec.groupdict()['id']
 
 # make sure to add the last section info.
 if id:
-    info.append((id, pars, tables, lists))
+    info.append((id, i+1, pars, tables, lists))
     pars = 0; tables = 0; lists = 0
 
 f = open('ug-stats.txt', 'w')
+f.write("# section number, section starts at line, paragraphs in this section, tables in this section, tables in this section.\n")
 for i in info:
-    f.write("%s paragraphs:%d, tables:%d, lists:%d\n\n" %(i[0],i[1], i[2], i[3] ))
+    f.write("%s sectionStart:%d paragraphs:%d, tables:%d, lists:%d\n\n" %(i[0],i[1], i[2], i[3], i[4] ))
 f.close()
 
 
