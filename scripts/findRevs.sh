30f234fcd39fab9c2772a2be00dab1827ce0da7c
mh1985 <mh1985@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2011-12-29 15:12:37 +0000

    script: findrevs.sh undo 1969 unintentionally modified script.

--- scripts/findRevs.sh
+++ scripts/findRevs.sh
@@ -1,45 +1,87 @@
+#!/bin/bash
 
-$BZR log -r $i >${rel}/log.txt
-cp $tempFile $targetFile
-cp $targetFile ${rel}/$origFile
-#### add stats file for userGuide
-if [ "$origFile" == "userGuide.t2t" ]; then
-pushd $rel >/dev/null 2>&1
-python ../../../scripts/stats.py
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+msg=""
+helperMsg=""
 
-git add ../../ug-stats-diff.txt
-popd >/dev/null 2>&1
+# apps that we need.
+
+BZR=`which bzr`
+DIFF=`which diff`
+WDIFF=`which wdiff`
+TWIDGE=`which twidge`
+
+if [ "$BZR" == "" ]; then
+    echo "bzr not installed."
+    exit
+elif [ "$WDIFF" == "" ]; then
+    echo "wdiff not installed."
+    exit
+elif [ "$DIFF" == "" ]; then
+    echo "diff not installed."
+    exit
+elif [ "$TWIDGE" == "" ]; then
+    echo "twidge not installed."
+    exit
 fi
-git add $rel
-fi
-i=$(($i+1))
-done
-echo -e "\nall done, checked for revision diffrences upto and including $endRev"
-rm $targetFile
+
+# convert relative to absolute paths
+# store in global var $absPath
+
+function getAbsPath() {
+absPath=`readlink -f -n $1`
 }
 
-
-
-function helper() {
+# function that does the work.
+function findRevs() {
 lang=$1
+fpath=$2
+diffsDir=$3
+fname=$4
 newRevs=""
-startRev=`ls -1 ../../../../${lang}/$DIFFSDIR/ | tail -n 1`
+echo "processing $lang: $fname"
+startRev=`ls -1 ${lang}/$diffsDir/ | tail -n 1`
 if [ "$startRev" == "disabled" ]; then
 return
 fi
-
-echo "my startRev is: $startRev"
-findRevs $lang
-count=`git status 2>/dev/null | grep "$lang/$DIFFSDIR" | wc -l`
-divisor=4
-if [ "$DIFFSDIR" == "ug-diffs" ]; then divisor=5; fi
-count=$(($count/$divisor))
-if [ "$count" != "0" ]; then
-helperMsg="$count in $DIFFSDIR ($newRevs)"
+startRev=$(($startRev+1))
+#echo "my startRev is: $startRev"
+newRevs=`bzr log -r${startRev}.. $BZRDIR/$fpath/$fname | grep -P "^revno: [0-9]+$" | sort | awk '{printf("%d ", $2)}'`
+echo "revs to be processed: $newRevs"
+prevRev=$startRev
+revCounter=0
+newRevs=($newRevs)
+for rev in ${newRevs[*]}; do
+#echo "processing $rev"
+mkdir -p $lang/$diffsDir/$rev
+bzr log -r$rev $BZRDIR/$fpath/$fname > $lang/$diffsDir/$rev/log.txt
+bzr cat -r$rev $BZRDIR/$fpath/$fname > $lang/$diffsDir/$rev/$fname
+bzr diff -r$prevRev..$rev $BZRDIR/$fpath/$fname > $lang/$diffsDir/$rev/diff.txt
+bzr diff -r$prevRev..$rev $BZRDIR/$fpath/$fname |
+$WDIFF -w '-{' -x '}-' -y '+{' -z '}+' -d |
+sed -e 's/-{/\n-{/g' -e 's/}-/}-\n/g' \
+-e 's/+{/\n+{/g' -e 's/}+/}+\n/g' > $lang/$diffsDir/$rev/wdiff.txt
+if [ "$diffsDir" == "ug-diffs" ]; then
+pushd $lang/$diffsDir/$rev/ >/dev/null
+python ../../../scripts/stats.py
+# update ug-stats-diff at the same time
+diff  --unchanged-line-format='' --old-line-format='en %L' --new-line-format="$lang %L" \
+ug-stats.txt ../../ug-stats.txt |
+sed -e "s/$lang $//g" -e "s/^en $//g" | sort -V -s -k 2,2 | 
+sed '/^\s*$/d' >../../ug-stats-diff.txt
+git add ../../ug-stats-diff.txt
+popd >/dev/null
+fi
+git add $lang/$diffsDir/$rev
+revCounter=$(($revCounter+1))
+done
+if [ "$revCounter" != "0" ]; then
+helperMsg="$revCounter in $diffsDir (${newRevs[*]})"
 else
 helperMsg=''
 fi
 }
+
 
 
 ## config
@@ -48,15 +90,12 @@ fi
 git svn rebase
 
 # go to relative dir that has bzr code:
-getAbsPath code/user_docs/en/
+getAbsPath ../
 pushd $absPath 2>&1 >/dev/null
-
-bzr pull >/dev/null 2>&1 # http://bzr.nvaccess.org/nvda/main
-endRev=`bzr log | grep revno | head -n 1 | awk '{print $2}'`
-if [ "$endRev" == "" ]; then
-    echo "could not find end revision, quitting."
-    exit
-fi
+BZRDIR=scripts/code
+pushd $BZRDIR
+bzr pull
+popd
 
 declare -A twitAddr
 twitAddr[ar]="@nvdauser"
@@ -66,13 +105,9 @@ twitAddr[sk]="@pvagner"
 langs=(ar de es fi fr gl it ja nl pl pt_BR sk ta tr)
 for lang in ${langs[*]}; do
     echo "processing $lang"
-    origFile=changes.t2t
-    DIFFSDIR=ch-diffs
-    helper $lang
+    findRevs $lang user_docs/en/ ch-diffs changes.t2t
     msgP1="$helperMsg"
-    origFile=userGuide.t2t
-    DIFFSDIR=ug-diffs
-    helper $lang
+    findRevs $lang user_docs/en/ ug-diffs userGuide.t2t
     msgP2="$helperMsg"
     # make sure the format looks nice.
     newMsg=''
