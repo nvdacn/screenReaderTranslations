1f7d2a9c552f33aa58d7c99f168be20f1f381a97
mh1985 <mh1985@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2012-03-08 22:37:20 +0000

    scripts: convertT2t.sh add bg to list of langs to be processed.

--- scripts/convertT2t.sh
+++ scripts/convertT2t.sh
@@ -12,7 +12,7 @@ absPath=`readlink -f -n $0`
 absPath=`dirname $absPath`
 pushd ${absPath}/../ >/dev/null 2>&1
 
-langs=(ar de es fi fr gl hu it ja nl nb_NO pl pt_BR pt_PT sk ta tr)
+langs=(ar bg de es fi fr gl hu it ja nl nb_NO pl pt_BR pt_PT sk ta tr)
 for lang in ${langs[*]}; do
     echo "processing $lang"
     cd $lang
