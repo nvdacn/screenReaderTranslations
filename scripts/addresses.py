7f7b31638644751eb21260a81e5bc14789db8f9d
mh1985 <mh1985@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2012-03-24 13:36:14 +0000

    scripts: addresses should only be defined in one place. Also added sconstruct file used to push things to bzr.

---
+++ scripts/addresses.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+addresses = {
+    'all': {
+        'lang': 'all',
+        'email': ['Mesar hameed <mesar.hameed+srt@gmail.com>'], 
+    },
+    'ar': { 
+        'lang':'Arabic',
+        'email': ['Fatma Mehanna <fatma.mehanna@gmail.com>'],
+    },
+    'bg': {
+        'lang':'Bulgarian',
+        'email': ['Zahari Yurukov <zahari.yurukov@gmail.com>'],
+    },
+    'da': {
+        'lang': 'Danish',
+        'email': ['Daniel K. Gartmann <kontakt@nvda.dk>'],
+    },
+    'de': {
+        'lang':'German',
+        'email': ['Bernd Dorer <bernd_dorer@yahoo.de>', 'David Parduhn <xkill85@gmx.net>'],
+    },
+    'es': {
+        'lang': 'Spanish',
+        'email': ['Juan C. buno <quetzatl@eresmas.net>'],
+    },
+    'fi': {
+        'lang':'Finnish',
+        'email': ['Jani Kinnunen <jani.kinnunen@wippies.fi>'],
+    }, 
+    'fr': {
+        'lang':'French',
+        'email': ['Michel such <michel.such@free.fr>'],
+    },
+    'gl': {
+        'lang': 'Galician',
+        'email': ['Juan C. buno <quetzatl@eresmas.net>'],
+    },
+    'hr': {
+        'lang': 'Croatian',
+        'email': ['Hrvoje Katić <hrvojekatic@gmail.com>', 'Mario Percinic <mario.percinic@gmail.com>'],
+    },
+    'hu': {
+        'lang': 'Hungarian',
+        'email': ['Áron Ócsvári <oaron1@gmail.com>'],
+    },
+    'it': {
+        'lang':'Italian',
+        'email': ['Simone Dal Maso <simone.dalmaso@juvox.it>'],
+    },
+    'ja': {
+        'lang':'Japanese',
+        'email': ['Katsutoshi Tsuji <tsuji-katsutoshi@mitsue.co.jp>'],
+    },
+    'nb_NO': {
+        'lang':'Norwegian bokmål',
+        'email': ['David Hole <balubathebrave@gmail.com>', 'Bjørnar Seppola <bjornar@seppola.net>'],
+    },
+    'nl': {
+        'lang':'Duch',
+        'email': ['Bram Duvigneau <bram@bramd.nl>'],
+    },
+    'pl': {
+        'lang':'Polish',
+        'email': ['Hubert Meyer <killer@tyflonet.com>'],
+    },
+   'pt_BR': {
+        'lang': 'Brazilian Portuguese',
+        'email': ['Cleverson Casarin Uliana <clever92000@yahoo.com.br>', 'Marlin Rodrigues <marlincgrodrigues@yahoo.com.br>'],
+    },
+    'pt_PT': {
+        'lang': 'Portuguese',
+        'email': ['Diogo Costa <diogojoca@gmail.com>'],
+    },
+    'sv': {
+        'lang':'Swedish',
+        'email': ['Mesar Hameed <mhameed@src.gnome.org>'],
+    },
+    'ta': {
+        'lang':'Tamil',
+        'email': ['Dinakar T.D. <td.dinkar@gmail.com>'],
+    },
+    'tr': {
+        'lang':'Turkish',
+        'email': ['Cagri Dogan <cagrid@hotmail.com>'],
+    },
+    'zh_CN': {
+        'lang':'Simplified Chinese',
+        'email': ['杨永全 <qt06.com@139.com>'],
+    },
+    'zh_HK': {
+        'lang': 'Traditional Chinese Hong Kong',
+        'email': ['Eric Yip <ericycy@gmail.com>'],
+    },
+    'zh_TW': {
+        'lang': 'Traditional Chinese Taiwan',
+        'email': ['wangjanli@gmail.com', 'maro.zhang@gmail.com'],
+    },
+}
+
