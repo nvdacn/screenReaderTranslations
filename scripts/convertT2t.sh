4059366ee934d41ca643b0c805dab63e79585fc5
mh1985 <mh1985@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2012-03-24 16:21:57 +0000

    scripts: convertT2t.sh send emails when t2t files are not UTF-8 encoded.

--- scripts/convertT2t.sh
+++ scripts/convertT2t.sh
@@ -16,23 +16,28 @@ langs=(ar bg de es fi fr gl hu it ja nl nb_NO pl pt_BR pt_PT sk ta tr)
 for lang in ${langs[*]}; do
     echo "processing $lang"
     cd $lang
-    python ../scripts/stats.py
-    $PYTHON27 ../scripts/generate.py
+    encoding=`file *.t2t  | grep -vi "utf-8"`
+    if [ "$encoding" != "" ]; then
+        python ../scripts/addresses.py $lang "File encoding problem" "Please save the following as unicode UTF-8: $encoding"
+    else
+        python ../scripts/stats.py
+        $PYTHON27 ../scripts/generate.py
 
-    lastRev=`ls -1 ug-diffs/ | tail -n 1`
-    diff  --unchanged-line-format='' --old-line-format='en %L' --new-line-format="$lang %L" \
-    ug-diffs/$lastRev/ug-stats.txt  ug-stats.txt |
-    sed -e "s/$lang $//g" -e "s/^en $//g" | sort -V -s -k 2,2 |
-    sed '/^\s*$/d' >ug-stats-diff.txt
-    mfiles=`git status -s -uno | grep -i ".html$" | awk '{printf(" %s", $2)}'`
-    mstats=`git status -s -uno | grep -i "ug\-stats\-diff.txt$" | awk '{printf(" %s", $2)}'`
+        lastRev=`ls -1 ug-diffs/ | tail -n 1`
+        diff  --unchanged-line-format='' --old-line-format='en %L' --new-line-format="$lang %L" \
+        ug-diffs/$lastRev/ug-stats.txt  ug-stats.txt |
+        sed -e "s/$lang $//g" -e "s/^en $//g" | sort -V -s -k 2,2 |
+        sed '/^\s*$/d' >ug-stats-diff.txt
+        mfiles=`git status -s -uno | grep -i ".html$" | awk '{printf(" %s", $2)}'`
+        mstats=`git status -s -uno | grep -i "ug\-stats\-diff.txt$" | awk '{printf(" %s", $2)}'`
 
-    if [ "$mfiles" != "" ]; then git add $mfiles; fi
-    if [ "$mstats" != "" ]; then git add $mstats ug-stats.txt; fi
-    if [ "$mfiles" != "$mstats" ]; then
-        msg="${lang}: updated $mfiles $mstats from t2t."
-        git commit -q -m "$msg"
-        ../scripts/commit.sh
+        if [ "$mfiles" != "" ]; then git add $mfiles; fi
+        if [ "$mstats" != "" ]; then git add $mstats ug-stats.txt; fi
+        if [ "$mfiles" != "$mstats" ]; then
+            msg="${lang}: updated $mfiles $mstats from t2t."
+            git commit -q -m "$msg"
+            ../scripts/commit.sh
+        fi
     fi
     cd ..
 done
