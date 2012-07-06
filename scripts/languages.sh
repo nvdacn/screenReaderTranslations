328e4693159ce202ec1d222133a7685cf7281538
mhameed <mhameed@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2012-07-06 21:56:12 +0000

    added empty nepali character descriptions file, added empty userGuide.t2t, and added english userGuide in ug-diffs, activated automatic t2t to html conversion for nepali, enabled automatic merge of new messages for nepali nvda.po file.

--- scripts/languages.sh
+++ scripts/languages.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # languages to be processed by the t2t to html convert script (convertT2t.sh).
-convertLangs=(ar bg cs de el es fi fr gl hu it ja nl nb_NO pl pt_BR pt_PT sk ta tr zh_TW)
+convertLangs=(ar bg cs de el es fi fr gl hu it ja ne nl nb_NO pl pt_BR pt_PT sk ta tr zh_TW)
 
 # languages to be processed by the findRevs.sh script.
 findRevLangs=(ar cs de es fi fr gl hu it ja nl nb_NO pl pt_BR pt_PT sk ta tr zh_TW)
 
 # languages to be processed by the updatePo.sh script.
-updatePoLangs=(am ar bg cs da de el es fi fr gl hr hu it ja nl nb_NO nn_NO pl pt_BR pt_PT sk sv ta tr uk zh_CN zh_HK zh_TW)
+updatePoLangs=(am ar bg cs da de el es fi fr gl hr hu it ja ne nl nb_NO nn_NO pl pt_BR pt_PT sk sv ta tr uk zh_CN zh_HK zh_TW)
 
