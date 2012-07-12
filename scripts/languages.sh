cff8b8074f58bf5e9d2b3970d387f75f04b23e79
mhameed <mhameed@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2012-07-12 08:47:27 +0000

    scripts/languages.sh add korean to the list of languages to be processed by findrevs.

--- scripts/languages.sh
+++ scripts/languages.sh
@@ -4,7 +4,7 @@
 convertLangs=(ar bg cs de el es fi fr gl hu it ja ko ne nl nb_NO pl pt_BR pt_PT sk ta tr zh_TW)
 
 # languages to be processed by the findRevs.sh script.
-findRevLangs=(ar cs de es fi fr gl hu it ja nl nb_NO pl pt_BR pt_PT sk ta tr zh_TW)
+findRevLangs=(ar cs de es fi fr gl hu it ja ko nl nb_NO pl pt_BR pt_PT sk ta tr zh_TW)
 
 # languages to be processed by the updatePo.sh script.
 updatePoLangs=(am ar bg cs da de el es fi fr gl hr hu it ja ko ne nl nb_NO nn_NO pl pt_BR pt_PT sk sv ta tr uk zh_CN zh_HK zh_TW)
