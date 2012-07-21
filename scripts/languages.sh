dc30ca742d209c3abfc2ff3a5215d4a5f191a366
mhameed <mhameed@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2012-07-21 09:59:52 +0000

    turn on automatic t2t to html conversion for amharic.

--- scripts/languages.sh
+++ scripts/languages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # languages to be processed by the t2t to html convert script (convertT2t.sh).
-convertLangs=(ar bg cs de el es fi fr gl hu it ja ko ne nl nb_NO pl pt_BR pt_PT sk ta tr zh_TW)
+convertLangs=(am ar bg cs de el es fi fr gl hu it ja ko ne nl nb_NO pl pt_BR pt_PT sk ta tr zh_TW)
 
 # languages to be processed by the findRevs.sh script.
 findRevLangs=(ar cs de es fi fr gl hu it ja ko nl nb_NO pl pt_BR pt_PT sk ta tr zh_TW)
