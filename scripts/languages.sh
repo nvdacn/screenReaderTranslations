e4ec831d2be86c858cb3133075321b44d1b8903d
mhameed <mhameed@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2012-10-01 06:01:02 +0000

    languages.sh: ru nvda.po should me automatically updated.

--- scripts/languages.sh
+++ scripts/languages.sh
@@ -7,5 +7,5 @@ convertLangs=(am ar bg cs de el es fi fr gl hu it ja ko ne nl nb_NO pl pt_BR pt_
 findRevLangs=(ar cs de es fi fr gl hu it ja ko nl nb_NO pl pt_BR pt_PT sk ta tr zh_TW)
 
 # languages to be processed by the updatePo.sh script.
-updatePoLangs=(am ar bg cs da de el es fi fr gl hr hu it ja ko ne nl nb_NO nn_NO pl pt_BR pt_PT sk sv ta tr uk zh_CN zh_HK zh_TW)
+updatePoLangs=(am ar bg cs da de el es fi fr gl hr hu it ja ko ne nl nb_NO nn_NO pl pt_BR pt_PT ru sk sv ta tr uk zh_CN zh_HK zh_TW)
 
