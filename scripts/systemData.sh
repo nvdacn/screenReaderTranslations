09c7cc751ec30e5ab791b97ed0894f93d361cb19
mhameed <mhameed@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2013-03-13 15:29:37 +0000

    systemData.sh remove dash from addon name.

--- scripts/systemData.sh
+++ scripts/systemData.sh
@@ -9,4 +9,4 @@ findRevLangs=(an ar cs da de es fi fr gl hu it is ja ko ne nl nb_NO pl pt_BR pt_
 # languages to be processed by the updatePo.sh script.
 updatePoLangs=(am an ar bg cs da de el es fi fr gl hi hr hu it is ja ko ne nl nb_NO nn_NO pl pt_BR pt_PT ru sk sl sv ta tr uk zh_CN zh_HK zh_TW)
 
-availableAddons=(dropbox extendedWinamp instantTranslate resourceMonitor systrayList unicodeBrailleInput vocalizer-driver word)
+availableAddons=(dropbox extendedWinamp instantTranslate resourceMonitor systrayList unicodeBrailleInput vocalizerDriver word)
