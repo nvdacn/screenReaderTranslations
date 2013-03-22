c966691dd5d2c23619e124b6521256644a607706
mhameed <mhameed@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2013-03-22 07:55:53 +0000

    systemData.sh: enable ga for automatic t2t to html conversion.

--- scripts/systemData.sh
+++ scripts/systemData.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # languages to be processed by the t2t to html convert script (convertT2t.sh).
-convertLangs=(am an ar bg cs da de el es fi fr gl hu it is ja ko ne nl nb_NO pl pt_BR pt_PT ru sl sk ta tr uk zh_TW)
+convertLangs=(am an ar bg cs da de el es fi fr ga gl hu it is ja ko ne nl nb_NO pl pt_BR pt_PT ru sl sk ta tr uk zh_TW)
 
 # languages to be processed by the findRevs.sh script.
 findRevLangs=(an ar cs da de es fi fr gl hu it is ja ko ne nl nb_NO pl pt_BR pt_PT sk ta tr zh_TW)
