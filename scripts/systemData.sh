ecc0ec7521dfec8efff76bf07ecce1c18ca48fd2
mhameed <mhameed@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2013-04-08 05:57:26 +0000

    systemData.sh: add bg to list of languages for which findRevs should be activated.

--- scripts/systemData.sh
+++ scripts/systemData.sh
@@ -4,7 +4,7 @@
 convertLangs=(am an ar bg cs da de el es fi fr ga gl hu it is ja ko ne nl nb_NO pl pt_BR pt_PT ru sl sk ta tr uk zh_TW)
 
 # languages to be processed by the findRevs.sh script.
-findRevLangs=(an ar cs da de es fi fr gl hu it is ja ko ne nl nb_NO pl pt_BR pt_PT sk ta tr zh_TW)
+findRevLangs=(an ar bg cs da de es fi fr gl hu it is ja ko ne nl nb_NO pl pt_BR pt_PT sk ta tr zh_TW)
 
 # languages to be processed by the updatePo.sh script.
 updatePoLangs=(am an ar bg cs da de el es fi fr gl hi hr hu it is ja ko ne nl nb_NO nn_NO pl pt_BR pt_PT ru sk sl sv ta tr uk zh_CN zh_HK zh_TW)
