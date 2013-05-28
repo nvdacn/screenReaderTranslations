1da4a6a8a341a9efe90759b973af4d7dff24c602
oaron <oaron@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2013-05-28 14:39:04 +0000

    Sam Broadcaster appmodule.

---
+++ hu/SAMBC.py
@@ -0,0 +1,18 @@
+import appModuleHandler
+import NVDAObjects.IAccessible
+class AppModule(appModuleHandler.AppModule):
+
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		windowClassName=obj.windowClassName
+		windowControlID=obj.windowControlID
+		if (windowClassName=="u'TVirtualSongTree'"
+			clsList.insert(0, Display)
+
+class Display(NVDAObjects.IAccessible.IAccessible):
+
+	shouldAllowIAccessibleFocusEvent=True
+	def _get_name(self):
+		name=super(Display,self).name
+		if not name:
+			name=_("Display")
+		return name
