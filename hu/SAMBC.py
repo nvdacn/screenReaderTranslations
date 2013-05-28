a52a40af174ad9d5e5135c199d7b11dc3211245e
mhameed <mhameed@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2013-05-28 20:10:46 +0000

    hu: SAMBC.py, attempt to force nvda to treat it as a list item.

--- hu/SAMBC.py
+++ hu/SAMBC.py
@@ -1,18 +1,21 @@
 import appModuleHandler
 import NVDAObjects.IAccessible
+from NVDAObjects.IAccessible import sysListView32
+
 class AppModule(appModuleHandler.AppModule):
 
-	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
-		windowClassName=obj.windowClassName
-		windowControlID=obj.windowControlID
+    def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+        windowClassName=obj.windowClassName
+        windowControlID=obj.windowControlID
         if windowClassName=="u'TVirtualSongTree'":
-			clsList.insert(0, Display)
+            obj.role=controlTypes.ROLE_LISTITEM
+            clsList.insert(0, sysListView32.ListItem))
 
 class Display(NVDAObjects.IAccessible.IAccessible):
 
-	shouldAllowIAccessibleFocusEvent=True
-	def _get_name(self):
-		name=super(Display,self).name
-		if not name:
-			name=_("Display")
-		return name
+    shouldAllowIAccessibleFocusEvent=True
+    def _get_name(self):
+        name=super(Display,self).name
+        if not name:
+            name=_("Display")
+        return name
