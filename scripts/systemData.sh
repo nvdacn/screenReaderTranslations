81143a89d64a0bb22d392244f1000443a496558f
mhameed <mhameed@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2013-06-02 09:47:07 +0000

    systemData: placemarker, moved so that it is in alphabetic order.

--- scripts/systemData.sh
+++ scripts/systemData.sh
@@ -9,4 +9,4 @@ findRevLangs=(an ar bg cs da de es fi fr gl hu it is ja ko ne nl nb_NO pl pt_BR 
 # languages to be processed by the updatePo.sh script.
 updatePoLangs=(am an ar bg cs da de el es fi fr gl hi hr hu it is ja ko ne nl nb_NO nn_NO pl pt_BR pt_PT ru sk sl sv ta tr uk zh_CN zh_HK zh_TW)
 
-availableAddons=(dropbox controlUsageAssistant extendedWinamp placeMarkers instantTranslate resourceMonitor systrayList unicodeBrailleInput vocalizerDriver word)
+availableAddons=(dropbox controlUsageAssistant extendedWinamp instantTranslate placeMarkers resourceMonitor systrayList unicodeBrailleInput vocalizerDriver word)
