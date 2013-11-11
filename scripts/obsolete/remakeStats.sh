bc1038f3967922756c46869089e19c26741d17e9
mhameed <mhameed@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2013-11-11 08:38:12 +0000

    reverted 12756.

--- scripts/obsolete/remakeStats.sh
+++ scripts/obsolete/remakeStats.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 lang=$1
-lastRev=`ls -1 userGuide-diffs/ | tail -n 1`
-diff  --unchanged-line-format='' --old-line-format='en %L' --new-line-format="$lang %L" \
-userGuide-diffs/$lastRev/userGuide-stats.txt  userGuide-stats.txt |
+diff  --unchanged-line-format='' --old-line-format='en %L' \
+--new-line-format="$lang %L" \
+userGuide-stats.txt ../../userGuide-stats.txt |
 sed -e "s/$lang $//g" -e "s/^en $//g" | sort -V -s -k 2,2 |
-sed '/^\s*$/d' >userGuide-stats-diff.txt
+sed '/^\s*$/d' >../../userGuide-stats-diff.txt
+
