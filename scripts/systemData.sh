a8da8c15ede82a748ccfd132fdc197467ad26e84
mhameed <mhameed@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2013-12-06 19:21:34 +0000

    scripts/systemData.sh: added in farsi.

--- scripts/systemData.sh
+++ scripts/systemData.sh
@@ -7,6 +7,6 @@ convertLangs=(am an ar bg cs da de el es fa fi fr ga gl hu it is ja ko ne nl nb_
 findRevLangs=(an ar bg cs da de es fi fr gl hu it is ja ko ne nl nb_NO pl pt_BR pt_PT sk ta tr zh_TW)
 
 # languages to be processed by the updatePo.sh script.
-updatePoLangs=(am an ar bg cs da de el es fi fr gl hi hr hu it is ja ko ne nl nb_NO nn_NO pl pt_BR pt_PT ru sk sl sv ta tr uk zh_CN zh_HK zh_TW)
+updatePoLangs=(am an ar bg cs da de el es fa fi fr ga gl hi hr hu it is ja ko ne nl nb_NO nn_NO pl pt_BR pt_PT ru sk sl sv ta th tr uk zh_CN zh_HK zh_TW)
 
 availableAddons=(dropbox controlUsageAssistant extendedWinamp goldwave instantTranslate placeMarkers resourceMonitor systrayList unicodeBrailleInput vocalizerDriver word)
