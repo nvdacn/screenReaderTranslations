7d15d01bad4095362d0773789ee7af066e16c887
mhameed <mhameed@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2013-12-06 15:22:22 +0000

    added new scripts/poStatus.sh

---
+++ scripts/poStatus.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+source systemData.sh
+
+
+printFileHeader() {
+echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">'
+echo '<HTML><HEAD>'
+echo '<META NAME="generator" CONTENT="postatus.sh">'
+echo '<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">'
+echo "<TITLE>po file status - $(date) </TITLE>"
+echo '</HEAD><BODY>'
+}
+
+printLangHeader() {
+    lang=$1
+    echo "<br/><h1>${lang}</h1><br/>"
+    echo "<table><tr><th>filename</th><th>percent translated</th><th>number of messages</th></tr>"
+}
+
+printLangFooter() {
+    echo "</table>"
+}
+
+printFileFooter() {
+echo '</BODY></HTML>'
+}
+
+function printRow() {
+file=$1
+pocount --csv $file | tail -n 1 | awk -F, '{print $1.$2.$9}' |
+sed -e 's+\.\./++g' -e 's+/+\&#47;+g' |
+awk '{printf("<tr><td>%s</td><td>%.2f%%</td><td>%d</td></tr>\n", $1, ($2/$3*100), $3)}'
+}
+
+printFileHeader
+for lang in ${updatePoLangs[*]}; do
+printLangHeader $lang
+find ../${lang}/ -iname "*.po" | sort | while read file; do
+printRow $file
+done
+find ../website/ -iname "*.${lang}.po" | sort | while read file; do
+printRow $file
+done
+printLangFooter
+done
+printFileFooter
