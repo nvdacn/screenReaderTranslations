73fb888d9e30da1a9e799bacb01cdb5590a479fd
mhameed <mhameed@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2014-08-02 08:29:45 +0000

    scripts/convertT2t.sh: sort numbers naturally within text, i.e. 99 should be before 100

--- scripts/convertT2t.sh
+++ scripts/convertT2t.sh
@@ -33,7 +33,7 @@ for lang in ${convertLangs[*]}; do
             txt2tags -q $file
         done
 
-        lastRev=`ls -1 userGuide-diffs/ | tail -n 1`
+        lastRev=`ls -1v userGuide-diffs/ | tail -n 1`
         diff  --unchanged-line-format='' --old-line-format='en %L' --new-line-format="$lang %L" \
         userGuide-diffs/$lastRev/userGuide-stats.txt  userGuide-stats.txt |
         sed -e "s/$lang $//g" -e "s/^en $//g" | sort -V -s -k 2,2 |
