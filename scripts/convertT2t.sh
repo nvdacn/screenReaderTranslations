049cfbae343132db79e6598bf0225eda376a7e0a
mhameed <mhameed@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2014-08-07 10:18:42 +0000

    scripts/convertT2t.sh split into convertOne.sh.

--- scripts/convertT2t.sh
+++ scripts/convertT2t.sh
@@ -20,29 +20,6 @@ pushd ${absPath}/../ >/dev/null 2>&1
 for lang in ${convertLangs[*]}; do
     echo "processing $lang"
     cd $lang
-    encoding=`file *.t2t  | grep -viP "utf-8|empty|ascii"`
-    if [ "$encoding" != "" ]; then
-        python ../scripts/addresses.py $lang "File encoding problem" "Please save the following as unicode UTF-8: $encoding"
-    else
-        $PYTHON27 ../scripts/keyCommandsDoc.py
-
-        # process each t2t file individually to make it easier to spot errors in output.
-        ls -1 *.t2t | while read file; do
-            echo processing $file
-            txt2tags -q $file
-        done
-
-        ../scripts/rebuildStats.sh
-
-        mfiles=`git status -s -uno | grep -i ".html$" | awk '{printf(" %s", $2)}'`
-        mstats=`git status -s -uno . | grep -i "structuredifferences.txt" | awk '{printf(" %s", $2)}'`
-
-        if [ "$mfiles" != "" ]; then git add $mfiles; fi
-        if [ "$mfiles" != "$mstats" ]; then
-            msg="${lang}: updated $mfiles $mstats from t2t."
-            git commit -q -m "$msg"
-            ../scripts/commit.sh
-        fi
-    fi
+    ../scripts/convertOne.sh
     cd ..
 done
