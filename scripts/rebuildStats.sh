ed039457689d1db0712ab1638e51938785d3320d
mhameed <mhameed@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2014-09-13 12:46:09 +0000

    scripts/rebuildStats.sh: path correction after directory rename.

--- scripts/rebuildStats.sh
+++ scripts/rebuildStats.sh
@@ -9,13 +9,13 @@ fi
 # Get the structure of the localized document
 python ../scripts/stats.py userGuide.t2t >localized.stats
 # Get the structure of the next revision to be translated.
-python ../scripts/stats.py userGuide-diffs/$nextRev/userGuide.t2t >next.stats
+python ../scripts/stats.py userGuide-newRevisions/$nextRev/userGuide.t2t >next.stats
 lang=$(basename `pwd`)
 
 d_args=(-d --unchanged-line-format='' --old-line-format='en %L' --new-line-format="$lang %L")
 sed_args=(
  # insert header line
- -e "1i# Structural comparison of ${lang}/userGuide.t2t against ${lang}/userGuide-diffs/$nextRev/userGuide.t2t"
+ -e "1i# Structural comparison of ${lang}/userGuide.t2t against ${lang}/userGuide-newRevisions/$nextRev/userGuide.t2t"
  # delete lines that only have the language tag.
  -e "/\(en\|${lang}\)\s*$/d"
  # insert footer line
@@ -25,7 +25,7 @@ sed_args=(
 if ! diff -q "${d_args[@]}" next.stats localized.stats; then
     diff "${d_args[@]}" next.stats localized.stats | sort -V -s -k 2,2 | sed "${sed_args[@]}" >${outFile}
 else
-    echo -e "# Structural comparison of ${lang}/userGuide.t2t against ${lang}/userGuide-diffs/$nextRev/userGuide.t2t\n# files have identical structure" >${outFile}
+    echo -e "# Structural comparison of ${lang}/userGuide.t2t against ${lang}/userGuide-newRevisions/$nextRev/userGuide.t2t\n# files have identical structure" >${outFile}
 fi
 
 rm localized.stats next.stats
