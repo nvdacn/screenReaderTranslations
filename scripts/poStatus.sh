2511c2c4fafb3e8eed1ef317abe852e18a79ac5a
jteh <jteh@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2014-11-10 07:36:09 +0000

    scripts/poStatus.sh: Handle total messages being 0 (probably due to broken po files) instead of barfing with awk divide by 0 errors.

--- scripts/poStatus.sh
+++ scripts/poStatus.sh
@@ -31,7 +31,7 @@ function printRow() {
 file=$1
 pocount --csv $file | tail -n 1 | awk -F, '{print $1.$2.$9}' |
 sed -e 's+\.\./++g' -e 's+/+\&#47;+g' |
-awk '{printf("<tr><td>%s</td><td>%.2f%%</td><td>%d</td></tr>\n", $1, ($2/$3*100), $3)}'
+awk '{printf("<tr><td>%s</td><td>%.2f%%</td><td>%d</td></tr>\n", $1, ($3==0)?0:($2/$3*100), $3)}'
 }
 
 printFileHeader
