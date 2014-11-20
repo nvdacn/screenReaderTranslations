fb5733931b5e9b2ed4e3ac61761ba8cf2074f782
mhameed <mhameed@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2014-11-20 23:26:30 +0000

    scripts/systemData.sh: add es_CO to list of languages that should be converted.

--- scripts/systemData.sh
+++ scripts/systemData.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # languages to be processed by the t2t to html convert script (convertT2t.sh).
-convertLangs=(am an ar bg ca cs da de el es fa fi fr ga gl hu it is ja ko ne nl nb_NO pl pt_BR pt_PT ru sk sl sr sv ta tr uk vi zh_CN zh_TW)
+convertLangs=(am an ar bg ca cs da de el es es_CO fa fi fr ga gl hu it is ja ko ne nl nb_NO pl pt_BR pt_PT ru sk sl sr sv ta tr uk vi zh_CN zh_TW)
 
 # languages to be processed by the findRevs.sh script.
 findRevLangs=(an ar bg cs da de es fi fr gl hu it is ja ko ne nl nb_NO pl pt_BR pt_PT sk ta tr zh_TW)
