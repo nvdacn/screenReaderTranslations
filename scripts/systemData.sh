6c8b2c233614453afb69a82e44b1f257eabfcab5
jteh <jteh@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2015-11-26 23:10:06 +0000

    scripts: Remove some obsolete stuff.

--- scripts/systemData.sh
+++ scripts/systemData.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-# languages to be processed by the t2t to html convert script (convertT2t.sh).
-convertLangs=(am an ar bg ca cs da de el es es_CO fa fi fr ga gl he hr hu it is ja ko ne nl nb_NO pl pt_BR pt_PT ru sk sl sr sv ta tr uk vi zh_CN zh_TW)
-
-# languages to be processed by the findRevs.sh script.
-findRevLangs=(an ar bg cs da de es fi fr gl hr hu it is ja ko ne nl nb_NO pl pt_BR pt_PT sk ta tr zh_TW)
-
 # languages to be processed by the updatePo.sh script.
 updatePoLangs=(am an ar bg cs da de el es es_CO fa fi fr ga gl hi hr hu it is ja ko ne nl nb_NO pa pl pt_BR pt_PT ru sk sl sr sv ta th tr uk zh_CN zh_HK zh_TW)
