faacace9aaf23c8898fc45433f96202b8b25ff1b
bdorer <bdorer@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2016-01-25 20:43:52 +0000

    addresses.py: changed email address for Cleverson as requested by mail.

--- scripts/addresses.py
+++ scripts/addresses.py
@@ -138,7 +138,7 @@ addresses = {
     },
     'pt_BR': {
         'lang': 'Brazilian Portuguese',
-        'email': ['Cleverson Casarin Uliana <clcaul@live.com>', 'Marlin Rodrigues <marlincgrodrigues@yahoo.com.br>'],
+        'email': ['Cleverson Casarin Uliana <clever97@gmail.com>', 'Marlin Rodrigues <marlincgrodrigues@yahoo.com.br>'],
     },
     'pt_PT': {
         'lang': 'Portuguese',
