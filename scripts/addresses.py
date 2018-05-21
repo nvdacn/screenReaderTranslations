0ab7efd04191f1255958e9aaf594bfdd73e22706
adriani.botez <adriani.botez@gmail.com>
2018-05-21 14:33:23 +0000

    Updated adresses.py for simplified chinese

--- scripts/addresses.py
+++ scripts/addresses.py
@@ -226,7 +226,7 @@ addresses = {
     },
     'zh_CN': {
         'lang':'Simplified Chinese',
-        'email': ['vgjh2005@gmail.com'],
+        'email': ['vgjh2005@gmail.com', 'jiangtiandao901647@gmail.com', 'manchen0528@gmail.com', 'sysking1024@gmail.com', 'dingpengyu06@gmail.com'],
     },
     'zh_HK': {
         'lang': 'Traditional Chinese Hong Kong',
